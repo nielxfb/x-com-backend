79cb42d49d289f8a4f82579c52c27f8e1788a053
nielxfb <adamludnl@gmail.com>
2025-07-29 16:54:59 +0700

    feat: db connection

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db_user := os.Getenv("DB_USER")
+	db_password := os.Getenv("DB_PASSWORD")
+	db_host := os.Getenv("DB_HOST")
+	db_port := os.Getenv("DB_PORT")
+	db_name := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta", db_host, db_user, db_password, db_name, db_port)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 }
