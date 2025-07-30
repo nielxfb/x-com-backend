a4ca1b5a7b2c1f34c7e0afb2a6d0059b71bfab17
nielxfb <adamludnl@gmail.com>
2025-07-30 15:08:03 +0700

    feat: added init script for db

---
+++ init-db/init.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE "user-db";
+CREATE DATABASE "community-db";
+CREATE DATABASE "thread-db";
+CREATE DATABASE "messaging-db";
+CREATE DATABASE "notification-db";
+CREATE DATABASE "content-db";
